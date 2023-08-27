436e116a78be1843eb5341319f910d574b60d51e
Praveenraj002 <praveenbose264@gmail.com>
2023-08-27 20:10:12 +0530

    added mobile menu

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Footer from "./components/Footer";
 function App() {
   return (
     <>
-      <div>
+      <div className="transition-all">
         <div>
           <Navbar />
         </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { HiOutlineMenuAlt4 } from "react-icons/hi";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import MenuLinksSm from "./MenuLinksSm";
 
 const NavbarLg = () => {
@@ -34,22 +34,47 @@ const NavbarLg = () => {
 };
 const NavbarSm = () => {
   const [showMenu, setShowMenu] = useState(false);
+  const [hideText, setHideText] = useState(false);
 
   const menuHandler = () => {
     setShowMenu(!showMenu);
   };
 
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setHideText(true);
+      } else {
+        setHideText(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <>
       <div className="container mx-auto px-6">
         <div className="flex flex-col text-center items-center justify-center">
-          <h1 className="my-2 font-forum tracking-widest font-bold text-3xl">
+          <h1
+            className={`my-2 font-forum tracking-widest font-bold ${
+              hideText ? "text-xl" : "text-3xl"
+            }`}
+          >
             THE DESIGN ATELIER
           </h1>
-          <p className="capitalize font-sans font-light text-sm tracking-widest">
+          <p
+            className={`capitalize font-sans font-light text-sm tracking-widest transition duration-300 ease-in-out ${
+              hideText ? "hidden" : "block"
+            }`}
+          >
             TIMELESS INTERIORS
           </p>
-          <div className="my-4">
+          <div className={`${hideText ? "" : "my-4"}`}>
             <HiOutlineMenuAlt4
               className="text-3xl font-light"
               onClick={menuHandler}
